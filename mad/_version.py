bd9bca36b2daffb83b07aaafe0e4a5062213835c
Maheen Asim <136640315+Asimmaheen@users.noreply.github.com>
2023-06-17 18:36:04 -0500

    _version.py

--- mad/_version.py
+++ mad/_version.py
@@ -5,3 +5,4 @@ __version__ = '0.0.2'
 # 0.0.1: Updated Readme
 # 0.0.2: Fixed import in optimize test file
 # 0.0.3: Created BFS trial in Data_structure and random goal assigment.
+# 0.0.4: created _initial_random_assignment file with changes in goal random assigment. 
