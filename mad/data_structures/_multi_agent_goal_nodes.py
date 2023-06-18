ec7c7e83b30aa72b388824151743bfe4297ed044
Maheen Asim <136640315+Asimmaheen@users.noreply.github.com>
2023-06-17 20:20:59 -0500

    Updated_multi_agent_goal_nodes.py

--- mad/data_structures/_multi_agent_goal_nodes.py
+++ mad/data_structures/_multi_agent_goal_nodes.py
@@ -13,20 +13,16 @@ class GoalNode:
         Goal Name
     data : dict
         Dictonary containing key as agent and value as planning cost
+    time_needed: int
+           time required for a goal to completed.
     parents: List, default []
         List of Parent GoalNodes
     children: List, default []
         List of children GoalNodes
     """
-
-    def __init__(self,
-                 name: str, 
-                 data: Dict, 
-                 parents: List = [],
-                 children: List = []) -> None:
-        
-        self.name = name 
+    def __init__(self, name: str, data: Dict, time_needed: int = 0, parents: List = [], children: List = []) -> None:
+        self.name = name
         self.data = data
-
+        self.time_needed = time_needed
         self.parents = parents
         self.children = children
