03af6b33c2f2c2d1a9ce7e1da2d60c7c3db78534
Jonathan Young <jonatanyoung7718@gmail.com>
2023-06-18 18:48:08 -0500

    updated version number

--- mad/_version.py
+++ mad/_version.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 #change log:
 # 0.0.0: first version
@@ -6,3 +6,4 @@ __version__ = '0.0.4'
 # 0.0.2: Fixed import in optimize test file
 # 0.0.3: Created GoalNode and some functionality
 # 0.0.4: Updated GoalNode, Optimization, tests, but in one file
+# 0.0.5
