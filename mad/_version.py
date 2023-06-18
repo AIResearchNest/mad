c4f6ca3f90eea2304b8a5a208d685125d3ce252e
Maheen Asim <136640315+Asimmaheen@users.noreply.github.com>
2023-06-17 20:47:36 -0500

    Updated _version.py

--- mad/_version.py
+++ mad/_version.py
@@ -5,4 +5,5 @@ __version__ = '0.0.2'
 # 0.0.1: Updated Readme
 # 0.0.2: Fixed import in optimize test file
 # 0.0.3: Created BFS trial in Data_structure and random goal assigment.
-# 0.0.4: created _initial_random_assignment file with changes in goal random assigment. 
+# 0.0.4: Created _initial_random_assignment file with changes in goals random assigment to agents
+# 0.0.5: Created greedy.py file that is greedy approach to agents assignemnet to goals.
