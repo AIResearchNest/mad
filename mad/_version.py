9249e9488849d84303b54009708504617924bc1a
Maheen Asim <136640315+Asimmaheen@users.noreply.github.com>
2023-06-23 16:20:49 -0500

    Update _version.py

--- mad/_version.py
+++ mad/_version.py
@@ -4,4 +4,5 @@ __version__ = '0.0.2'
 # 0.0.0: first version
 # 0.0.1: Updated Readme
 # 0.0.2: Fixed import in optimize test file
-# 0.0.3: Pushed random goal allocation files and tests.
+# 0.0.3: Pushed random goal allocation files and optimization tests in optimize folder.
+# 0.0.4: Created a new file with agents' auctions and allocation to nodes with keeping track of resources in mad/mad folder.
