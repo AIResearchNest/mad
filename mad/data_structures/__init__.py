20137921eaf3923442b7973ead52df275c732729
Maheen Asim <asim1@stolaf.edu>
2023-07-05 10:57:09 -0500

    fixed __init__.py

--- mad/data_structures/__init__.py
+++ mad/data_structures/__init__.py
@@ -1,6 +1,6 @@
 from mad.data_structures._multi_agent_goal_nodes import GoalNode, level_order_transversal, print_goal_tree
-
+from mad.data_structures._multi_agent_goal_node_two import GoalNode, level_order_transversal_two
 __all__ = [
     'GoalNode',
-    'print_goal_tree','level_order_transversal'
+    'print_goal_tree','level_order_transversal', 'level_order_transversal_two'
 ]
--- mad/data_structures/__inittwo__.py
+++
@@ -1,5 +0,0 @@
-from mad.data_structures._multi_agent_goal_node_two import GoalNode, level_order_transversal_two
-__all__ = [
-    'GoalNode',
-    'level_order_transversal_two'
-]
