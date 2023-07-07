b316b139b41e81a1678ac5b2b4ebba0c47ce99f1
Jonathan Young <jonatanyoung7718@gmail.com>
2023-07-07 15:19:33 -0500

    altered dfs_allocation and random_tests again

--- mad/_version.py
+++ mad/_version.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.42'
+__version__ = '0.0.43'
 
 #change log:
 # 0.0.0: first version
@@ -44,3 +44,4 @@ __version__ = '0.0.42'
 # 0.0.40: added more tree test cases and 3d graphing
 # 0.0.41: Updated 3d graphing
 # 0.0.42: altered dfs_allocation and random_tests
+# 0.0.43: altered dfs_allocation and random_tests again
