cfdd1981852c6e40439f08bd1426d76e72271955
FayNguyen03 <ntkhanh391914@gmail.com>
2023-07-07 18:33:54 -0500

    Efficiency test

--- mad/tests/efficiency_test.py
+++ mad/tests/efficiency_test.py
@@ -1,349 +1,75 @@
-import time
-from typing import Dict
-import copy
-import random
-from mad.data_structures import GoalNode, level_order_transversal
-from mad.optimize._goal_allocation import dfs_goal_allocation, optimized_goal_allocation
-
-
-def _random_cost(m: int, n: int) -> Dict[str, int]:
-    
-    """
-    This function randomizes the cost of an agent when it conducts a goal based on an assigned range
-
-    Parameters
-    ----------
-    m: int
-        The starting point of the range
-    n: int
-        The ending point of the range
-    
-    Returns
-    -------
-    
-    Dict[str,int]
-        A dictionary with the agents as keys and corresponding costs as values
-    
-    """
-    
-    d = {}
-    d["grace"] = random.randint(m,n)
-    d["remus"] = random.randint(m,n)
-    d["franklin"] = random.randint(m,n)
-    return d
-
-# Test case
-def test_algorithm_efficiency():
-
-    #TEST CASE 1: SAME MAX RESOURCES
-
-    # Create a sample goal tree
-    print("Test case 1:\n")
-    initial_goal_tree = GoalNode("G1", _random_cost(50, 70))
-    # Add some goals and their costs to the goal tree
-    G2 = GoalNode("G2", _random_cost(25, 40))
-
-    G3 = GoalNode("G3",_random_cost(25,40))
-
-    G4 = GoalNode("G4",_random_cost(25,40))
-
-    G5 = GoalNode("G5",_random_cost(15,30))
-
-    G6 = GoalNode("G6",_random_cost(15,30))
-
-    G7 = GoalNode("G7",_random_cost(15,30))
-
-    G8 = GoalNode("G8",_random_cost(15,30))
-
-    G9 = GoalNode("G9",_random_cost(15,30))
-
-    G10 = GoalNode("G10",_random_cost(5,10))
-
-    G11 = GoalNode("G11",_random_cost(5,10))
-
-    G12 = GoalNode("G12",_random_cost(5,10))
-
-    G13 = GoalNode("G13",_random_cost(5,10))
-
-
-    initial_goal_tree.add_child(G2)
-    initial_goal_tree.add_child(G3)
-    initial_goal_tree.add_child(G4)
-
-    G2.add_child(G5)
-    G2.add_child(G7)
-    G3.add_child(G8)
-    G3.add_child(G6)
-    G3.add_child(G9)
-    
-    G4.add_child(G10)
-    G4.add_child(G11)
-    G4.add_child(G12)
-    G4.add_child(G13)
-
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-
-    # Set the maximum resources for each agent
-    #Every agent has the same max_resources
-    max_resources = [50,50,50] 
-
-    # Test jonathan_algorithm
-    print("Jonathan's algorithm \n")
-    start_time = time.time()
-    score = dfs_goal_allocation(goal_tree, max(max_resources),1)
-    dfs_goal_allocation_time = time.time() - start_time
-    print('\n' * 4)
-
-    # Test fay_initial_goal_allocation
-    print("Fay's algorithm \n")
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-    start_time = time.time()
-    if goal_tree is None:
-            return
-
-    q = []
-    q.append((goal_tree, None)) 
-
-    while len(q) != 0:
-        level_size = len(q)
-
-        while len(q) > 0:  
-            node, parent = q.pop(0)
-            node.initial_agent_assign()
-            children = node.get_children()
-            for child in children:
-                q.append((child, node)) 
-
-    optimized_goal_allocation(goal_tree, max_resources)
-    fay_initial_goal_allocation_time = time.time() - start_time
-
-    # Print the execution times
-    print("Execution Time - jonathan_algorithm:", dfs_goal_allocation_time)
-    print("Execution Time - fay_initial_goal_allocation:", fay_initial_goal_allocation_time)
-
-#TEST CASE 2: DIFFERENT MAX RESOURCES
-
-    # Create a sample goal tree
-    print("Test case 2:\n")
-    initial_goal_tree = GoalNode("G1", _random_cost(60, 80))
-    # Add some goals and their costs to the goal tree
-    G2 = GoalNode("G2", _random_cost(25, 45)) # level 2
-
-    G3 = GoalNode("G3",_random_cost(25,45))
-
-    G4 = GoalNode("G4",_random_cost(25,45))
-
-    G5 = GoalNode("G5",_random_cost(15,30)) # level 3
-
-    G6 = GoalNode("G6",_random_cost(15,30))
-
-    G7 = GoalNode("G7",_random_cost(15,30))
-
-    G8 = GoalNode("G8",_random_cost(15,30))
-
-    G9 = GoalNode("G9",_random_cost(15,30)) 
-
-    G10 = GoalNode("G10",_random_cost(5,15)) # level 4
-
-    G11 = GoalNode("G11",_random_cost(5,15))
-
-    G12 = GoalNode("G12",_random_cost(5,15))
-
-    G13 = GoalNode("G13",_random_cost(5,15))
-
-    G14 = GoalNode("G14",_random_cost(1,10)) # level 5
-
-    G15 = GoalNode("G15",_random_cost(1,10))
-
-    G16 = GoalNode("G16",_random_cost(1,10))
-
-
-    initial_goal_tree.add_child(G2)
-    initial_goal_tree.add_child(G3)
-    initial_goal_tree.add_child(G4)
-
-    G2.add_child(G5)
-    G2.add_child(G7)
-    G3.add_child(G8)
-    G3.add_child(G6)
-    G2.add_child(G9)
-
-    G6.add_child(G10)
-    G6.add_child(G13)
-    G9.add_child(G12)
-    G9.add_child(G11)
-    
-    G11.add_child(G14)
-    G11.add_child(G16)
-    G11.add_child(G15)
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-
-    # Set the maximum resources for each agent
-
-    #Every agent has the different max_resources
-    max_resources = [50,60,55] 
-
-    # Test jonathan_algorithm
-    print("Jonathan's algorithm \n")
-    start_time = time.time()
-    score = dfs_goal_allocation(goal_tree, max(max_resources),1)
-    dfs_goal_allocation_time = time.time() - start_time
-    print('\n' * 4)
-
-    # Test fay_initial_goal_allocation
-    print("Fay's algorithm \n")
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-    start_time = time.time()    
-
-    #Initialize the least agent cost for each goal
-    q = []
-    q.append((goal_tree, None)) # enqueue the root into the queue
-
-    while len(q) != 0:
-        level_size = len(q)
-
-        while len(q) > 0:  # Iterate over the current level
-            node, parent = q.pop(0)
-            node.initial_agent_assign()
-            children = node.get_children()
-            for child in children:
-                q.append((child, node))  # Add the children into the queue along with their parent
-
-    optimized_goal_allocation(goal_tree, max_resources)
-    fay_initial_goal_allocation_time = time.time() - start_time
-
-    # Print the execution times
-    print("Execution Time - jonathan_algorithm:", dfs_goal_allocation_time)
-    print("Execution Time - fay_initial_goal_allocation:", fay_initial_goal_allocation_time)
-    
-
-    print("Test case 3:\n")
-    initial_goal_tree = GoalNode("G1", _random_cost(40, 60))
-    # Add some goals and their costs to the goal tree
-    G2 = GoalNode("G2", _random_cost(20, 30)) # level 2
-
-    G3 = GoalNode("G3",_random_cost(20,30))
-
-    G4 = GoalNode("G4",_random_cost(20,30))
-
-    G5 = GoalNode("G5",_random_cost(10,15)) # level 3
-
-    G6 = GoalNode("G6",_random_cost(10,15))
-
-    G7 = GoalNode("G7",_random_cost(10,15))
-
-    G8 = GoalNode("G8",_random_cost(10,15))
-
-    G9 = GoalNode("G9",_random_cost(10,15)) 
-
-    G10 = GoalNode("G10",_random_cost(5,10)) # level 4
-
-    G11 = GoalNode("G11",_random_cost(5,10))
-
-    G12 = GoalNode("G12",_random_cost(5,10))
-
-    G13 = GoalNode("G13",_random_cost(5,10))
-
-    G14 = GoalNode("G14",_random_cost(1,5)) # level 5
-
-    G15 = GoalNode("G15",_random_cost(1,5))
-
-    G16 = GoalNode("G16",_random_cost(1,5))
-
-
-    initial_goal_tree.add_child(G2)
-    initial_goal_tree.add_child(G3)
-    initial_goal_tree.add_child(G4)
-
-    G2.add_child(G5)
-    G2.add_child(G7)
-    G3.add_child(G8)
-    G3.add_child(G6)
-    G2.add_child(G9)
-
-    G6.add_child(G10)
-    G6.add_child(G13)
-    G9.add_child(G12)
-    G9.add_child(G11)
-
-    G11.add_child(G14)
-    G11.add_child(G16)
-    G11.add_child(G15)
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-
-    # Set the maximum resources for each agent
-    # Every agent has different max_resources
-    max_resources = [30, 40, 35] 
-
-    # Test jonathan_algorithm
-    print("Jonathan's algorithm \n")
-    start_time = time.time()
-    score = dfs_goal_allocation(goal_tree, max_resources[0],1)
-    dfs_goal_allocation_time = time.time() - start_time
-    print('\n' * 4)
-
-    # Test fay_initial_goal_allocation
-    print("Fay's algorithm \n")
-
-    goal_tree = copy.deepcopy(initial_goal_tree)
-    start_time = time.time()
-
-    #Initialize the least agent cost for each goal
-
-    q = []
-    q.append((goal_tree, None)) 
-
-    while len(q) != 0:
-        level_size = len(q)
-
-        while len(q) > 0:  
-            node, parent = q.pop(0)
-            node.initial_agent_assign()
-            children = node.get_children()
-            for child in children:
-                q.append((child, node))  
-
-    optimized_goal_allocation(goal_tree, max_resources)
-    fay_initial_goal_allocation_time = time.time() - start_time
-
-    # Print the execution times
-    print("Execution Time - jonathan_algorithm:", dfs_goal_allocation_time)
-    print("Execution Time - fay_initial_goal_allocation:", fay_initial_goal_allocation_time)
-
-
-    print("Test case 4 (Edge case):\n")
-
-    max_resources = [30, 40, 35] # Every agent has different max_resources
-
-    # Test jonathan_algorithm
-    print("Jonathan's algorithm \n")
-    start_time = time.time()
-    score = dfs_goal_allocation(None, max(max_resources),1)
-    dfs_goal_allocation_time = time.time() - start_time
-    print('\n' * 4)
-
-    # Test fay_initial_goal_allocation
-    print("Fay's algorithm \n")
-
-    
-    start_time = time.time()
-
-    optimized_goal_allocation(None, max_resources)
-    fay_initial_goal_allocation_time = time.time() - start_time
-
-# Run the test cases
-test_algorithm_efficiency()
-
+#Fay's efficiency test cases
+#
+#RESOURCE UTILIZATION
+#
+#The less resources all agents use, the more efficient the algorithm is
+# Define the input data
+goal_tree = ...  # Define the goal tree
+max_resources = ...  # Define the maximum resources for each agent
+
+# Execute Jonathan's Algorithm
+result1 = initial_goal_allocation(goal_tree, max_resources)
+
+# Calculate total resource utilization for Jonathan's Algorithm
+total_utilization1 = sum(sum(goal.cost for goal in goals) for goals in result1.values())
+
+# Execute Fay's Algorithm
+result2 = optimized_goal_allocation(goal_tree, max_resources)
+
+# Calculate total resource utilization for Fay's Algorithm
+total_utilization2 = sum(sum(goal.cost for goal in goals) for goals in result2.values())
+
+# Execute Maheen's Algorithm
+result3 = perform_auction_m(goal_tree, max_resources)
+
+# Calculate total resource utilization for Maheen's Algorithm
+total_utilization3 = sum(goal.cost for goal in result3)
+
+# Compare total resource utilization
+print("Total resource utilization for J:", total_utilization1)
+print("Total resource utilization for F:", total_utilization2)
+print("Total resource utilization for M:", total_utilization3)
 """
-Fay's comment:
-Jonathan's algorithm if more efficient when the goal tree has a balanced structure and the resource constraints are not too tight.
+Plot idea: 
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-Fay's algorithm is more efficient when the goal tree has a skewed structure or when there are significant variations in the costs of goals. 
+# Define the algorithm names and total resource utilization values
+algorithm_names = ['Algorithm 1', 'Algorithm 2', 'Algorithm 3']
+total_utilization_values = [total_utilization1, total_utilization2, total_utilization3]
 
-Two algorithms all throw the exception in the edge case (empty goal tree)
+# Generate data for the agents
+agents = ['Agent 1', 'Agent 2', 'Agent 3']
+X, Y = np.meshgrid(np.arange(len(agents)), np.arange(len(algorithm_names)))
+Z = np.zeros((len(algorithm_names), len(agents)))
+
+# Fill the Z-axis values with the total resource utilization
+for i in range(len(algorithm_names)):
+    Z[i] = total_utilization_values[i]
+
+# Create the 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the bars
+dx = dy = 0.8
+dz = Z.flatten()
+ax.bar3d(X.flatten(), Y.flatten(), np.zeros_like(Z.flatten()), dx, dy, dz, color='skyblue')
+
+# Set the labels and title
+ax.set_xticks(np.arange(len(agents)))
+ax.set_xticklabels(agents)
+ax.set_yticks(np.arange(len(algorithm_names)))
+ax.set_yticklabels(algorithm_names)
+ax.set_xlabel('Agents')
+ax.set_ylabel('Algorithms')
+ax.set_zlabel('Total Resource Utilization')
+ax.set_title('Resource Utilization Comparison')
+
+# Rotate the plot for better visibility
+ax.view_init(azim=-60, elev=30)
+
+# Display the plot
+plt.show()
 """
