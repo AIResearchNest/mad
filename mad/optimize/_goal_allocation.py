601a5e3d6aaff6a34a98ccb326bbf6f7400dd116
FayNguyen03 <ntkhanh391914@gmail.com>
2023-08-02 00:54:48 -0500

    Added 08/02

--- mad/optimize/_goal_allocation.py
+++ mad/optimize/_goal_allocation.py
@@ -503,6 +503,20 @@ def _decision_algorithm(list_goal: List[GoalNode], i: int, max_res: Dict[str, in
         return i + 1, list_goal, max_res
 
 def calculate_tree_depth(node):
+    """
+        Find the depth of the goal tree
+
+        Parameters
+        ----------
+        node: GoalNode
+            The list of goal nodes to be achieved.
+
+        Returns
+        -------
+        
+        int
+            The depth of the goal tree
+    """
     # If the node is a leaf (has no children), its depth is 1
     if not node.children:
         return 1
@@ -1405,3 +1419,47 @@ def equal_node(node):
 Random Agent
 ########################################################
 """
+def _optimal_path(goal_tree: GoalNode, max_resources: int) -> List:
+    """
+    Takes in a root node to a GoalNode goal tree and a max resources int for all the agents and finds the most optimal goal path based on the GoalNode.cost values through out the tree and returns a list of GoalNodes that should be accomplished. The returned GoalNodes are selected by finding the cheapest possible solution to the goal tree.
+
+    Parameters
+    ----------
+    goal_tree : mad.data_structures.GoalNode
+        Hierarchical Multi Agent Goal Tree
+    max_resources : int
+        Integer value of max resources for each agent
+
+    Returns:
+    -------
+    selected_goals : List
+        List of GoalNodes to be accomplished by agents in the domain
+    """
+
+    # Break Case: if GoalNode has no children, return [goal_tree]
+    if not goal_tree.children:
+        return [goal_tree]
+    
+    # Goals that will be returned
+    selected_goals = []
+
+    # Current GoalNode's children GoalNodes
+    child_goals = []
+    for child in goal_tree.children:
+        child_goals.extend(_optimal_path(child, max_resources))
+    
+    # Finds total cost of all children GoalNodes for comparison
+    child_cost = sum(min(child.data.values()) for child in child_goals)
+    goal_tree_cost = min(goal_tree.data.values())
+    
+    # Checks that a goal cost is less than the max possible resources an agent can have ands that the current GoalNode is cheaper than it's children
+    if goal_tree_cost <= max_resources and goal_tree_cost < child_cost:
+        selected_goals.append(goal_tree)
+    # Otherwise add the children nodes
+    else:
+        selected_goals.extend(child_goals)
+    
+    return selected_goals
+
+def randomly_assigned(list_goal: GoalNode):
+    pass
